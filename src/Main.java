a7202a0e5e99e551bdb5f42effedaa24fbf4ed9b
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 12:43:15 -0400

    Condition627

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String name = "Zbdoul";
+
+        System.out.println(name.charAt(0));
+
+        if (name.charAt(0) == 'A') {
+            System.out.println("Name starts with A");
+        } else {
+            System.out.println("Name does not start with A");
+            System.out.println(mathOne());
+        }
+    }
+    static int mathOne() {
+        return 0;
     }
 }
